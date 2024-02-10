7bb77be307e4f7a813d77c9bff7082d45b4d889f
Teresa Muhiu <muhiutw9@gmail.com>
2024-02-10 17:29:28 -0500

    Added Django Authentication to Github Capstone project

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
+from django.urls import reverse
 # from .models import related models
 # from .restapis import related methods
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from datetime import datetime
 import logging
 import json
+
+
+
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -37,14 +42,38 @@ def contact(request):
 # Create a `login_request` view to handle sign in request
 # def login_request(request):
 # ...
-
+def login_request(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect(reverse('djangoapp:index'))  # Redirect to the homepage after successful login
+        else:
+            messages.error(request, 'Invalid username or password. Please try again.')
+    return render(request, 'djangoapp/login.html')
 # Create a `logout_request` view to handle sign out request
 # def logout_request(request):
 # ...
+def logout_request(request):
+    logout(request)
+    return redirect(reverse('djangoapp:index'))
 
 # Create a `registration_request` view to handle sign up request
 # def registration_request(request):
 # ...
+def signup_request(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect(reverse('djangoapp:index'))  # Redirect to the login page after successful signup
+    else:
+        form = UserCreationForm()
+    return render(request, 'djangoapp/registration.html', {'form': form})
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
