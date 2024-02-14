bbf557b53899af18adeacc2d41e2bc708360b04e
Teresa Muhiu <muhiutw9@github.com>
2024-02-14 11:13:49 -0500

    Added a dealership Bootstrap table

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -83,10 +83,12 @@ def get_dealerships(request):
         url = "https://muhiutw9-3000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/dealerships/get"
         # Get dealers from the URL
         dealerships = get_dealers_from_cf(url)
-        # Concat all dealer's short name
-        dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
-        # Return a list of dealer short name
-        return HttpResponse(dealer_names)
+        # Build a context dictionary to pass data to the template
+        context = {
+            'dealerships': dealerships,
+        }
+       # Render the template with the context data
+        return render(request, 'djangoapp/index.html', context)
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
